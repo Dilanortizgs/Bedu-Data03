9da42ae1a00ae1c6b783d7f428e7747efda9a717
Dilan Ortiz Granados <Dilan@MacBook-Air-de-Dilan.local>
2020-11-28 10:05:25 -0600

    Fist example

---
+++ 01_tabla_multiplicar.py
@@ -0,0 +1,15 @@
+#Preguntar el número que el usuario quería saber su tabla de multiplicar
+
+numero = input('¿Qué número quieres multiplicar?\t')
+numero = int(numero)
+
+print(f'A continuación se muestra la tabla del {numero}')
+print('-------------------------------------------------')
+
+for n in range(10)
+    indice = (n + 1) * numero
+    resultado = numero * indice
+    print(f'{numero} * {indice} = {resultado}')
+
+
+
